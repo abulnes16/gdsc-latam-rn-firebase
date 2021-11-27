b8c301286829547d36a72cb932a2c393f8ad001f
Angel Bulnes <angel_bulnes16@yahoo.com>
2021-11-27 08:31:55 -0600

    Add drawer button to home

--- app/@types/navigation.d.ts
+++ app/@types/navigation.d.ts
@@ -1,3 +1,4 @@
+import { DrawerScreenProps } from '@react-navigation/drawer';
 import { StackScreenProps } from '@react-navigation/stack';
 
 declare global {
@@ -14,3 +15,4 @@ declare global {
 
 /* Navigation Types */
 export type LoginScreenProps = StackScreenProps<AuthParamList, 'LoginScreen'>;
+export type HomeScreenProps = DrawerScreenProps<DrawerParamsList, 'HomeScreen'>;
